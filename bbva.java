46f4b079bca95969eb5392dd842999a260f23430
Scarlett Guerra <scarlettguerra458@gmail.com>
2024-11-16 10:38:16 -0600

    Segundo

--- bbva.java
+++ bbva.java
@@ -1,6 +1,18 @@
+class persona {
+
+    String nombre;
+    String sexo;
+    int edad;
+
+    persona(String nombre, String sexo, int edad) {
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.edad = edad;
+    }
+}
+
 class programa {
-    public static void main (String  args )´
-    {
+    public static void main(String[] args) {
         System.out.println("HOLA MUNDO ");
     }
 }
