cc485e2afb842db1ebd69ec83bc26c0660c74f9a
Scarlett Guerra <scarlettguerra458@gmail.com>
2024-11-16 12:46:50 -0600

    tercer commit

--- bbva.java
+++ bbva.java
@@ -11,6 +11,33 @@ class persona {
     }
 }
 
+class Cliente extends persona {
+    int numero;
+
+    Cliente(String nombre, String sexo, int edad) {
+        super(nombre, sexo, edad);
+        this.numero = numero;
+    }
+
+}
+
+class Banco {
+    String nombre;
+
+    Cliente clientes[];
+
+    Banco(int cantidadClientes) {
+        clientes = new Cliente[cantidadClientes];
+        this.nombre = nombre;
+    }
+
+    Cliente crearCliente(String nombre, String sexo, int edad, int numero) {
+        System.out.println("Creando cliente...");
+        Cliente cliente = new Cliente(nombre, sexo, edad, numero);
+        return cliente;
+    }
+}
+
 class programa {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO ");
